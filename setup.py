1f66753d7d1414e9c2bfd62890601a78015b8153
David Sheffield <david@davidsheffield.net>
2017-11-24 13:08:34 -0500

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, Extension
 
-here = path.abspath(path.dirname(__file__)
+here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'README.rst')) as f:
     readme = f.read()
