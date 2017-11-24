8a00a59d71808aee871144f2eb682624062d83ac
David Sheffield <david@davidsheffield.net>
2017-11-24 13:03:57 -0500

    Rework structure. * Create scripts/ directory * Use setuptools * Combine setup.py files into one * Add license * Use .rst for README and format * Include manifest

--- README.rst
+++ README.rst
@@ -7,27 +7,42 @@ Positions
 ---------
 
 0: Player stock
+
 1: Player hand
+
 2: Opponent stock
+
 3: Opponent waste
+
 4-11: Banks
+
 12-19: Tableau
 
-    2  3
-
-16  4  8 12
-17  5  9 13
-18  6 10 14
-19  7 11 15
-
-    1  0
++--+--+--+--+
+|  | 2| 3|  |
++--+--+--+--+
+|16| 4| 8|12|
++--+--+--+--+
+|17| 5| 9|13|
++--+--+--+--+
+|18| 6|10|14|
++--+--+--+--+
+|19| 7|11|15|
++--+--+--+--+
+|  | 1| 0|  |
++--+--+--+--+
 
 Move errors
 -----------
 
 0: Card moved, no error.
+
 1: Attempt to move card to original location.
+
 2: Locations out of range.
+
 3: Cannot move opponent's cards or bank cards, cannot move to players piles.
+
 4: Cannot move a card that is not there.
+
 5: Not a legal move.
--- setup.py
+++ setup.py
@@ -1,15 +1,64 @@
-from distutils.core import setup
-from distutils.extension import Extension
-import sys
+import os
 
-if len(sys.argv) < 2:
-    print("Needs and argument")
-else:
-    import russianbank.core.setup
-    russianbank.core.setup.setup_package(sys.argv[1:])
+from setuptools import setup, Extension
 
-    import russianbank.neuralnetwork.setup
-    russianbank.neuralnetwork.setup.setup_package(sys.argv[1:])
+here = path.abspath(path.dirname(__file__)
 
-# setup(name="russianbank",
-# )
+with open(os.path.join(here, 'README.rst')) as f:
+    readme = f.read()
+
+setup(name='Russian Bank',
+      version='0.1.0',
+      description='Russian Bank card game',
+      long_description=readme,
+      author='David Sheffield',
+      author_email='david@davidsheffield.net',
+      url='https://github.com/davidsheffield/RussianBank',
+      packages=['russianbank'],
+      license='GNU GPLv3',
+      install_requires=[''],
+      ext_modules=[
+          Extension("core_module",
+                    ["russianbank/core/src/CoreModule.cc",
+                     "russianbank/core/src/Field.cc",
+                     "russianbank/core/src/Card.cc",
+                     "russianbank/core/src/Player.cc"],
+                    include_dirs=['russianbank/'],
+                    libraries = ["boost_python3"],
+                    extra_compile_args=['-std=c++11','-stdlib=libc++'],
+                    extra_link_args=['-stdlib=libc++']
+          ),
+          Extension("network",
+                    ["russianbank/neuralnetwork/src/NeuralNetwork.cc"],
+                    include_dirs=['russianbank/'],
+                    libraries = ["boost_python3"],
+                    extra_compile_args=['-std=c++11','-stdlib=libc++'],
+                    extra_link_args=['-stdlib=libc++']
+          ),
+          Extension("match",
+                    ["russianbank/neuralnetwork/src/Match.cc"],
+                    include_dirs=['russianbank/'],
+                    libraries = ["boost_python3"],
+                    extra_compile_args=['-std=c++11','-stdlib=libc++'],
+                    extra_link_args=['-stdlib=libc++']
+          ),
+          Extension("evolve",
+                    ["russianbank/neuralnetwork/src/Evolve.cc"],
+                    include_dirs=['russianbank/'],
+                    libraries = ["boost_python3"],
+                    extra_compile_args=['-std=c++11','-stdlib=libc++'],
+                    extra_link_args=['-stdlib=libc++']
+          )
+      ],
+      keywords='russian bank card solitaire',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: MacOS X',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Natural Language :: English',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Games/Entertainment'
+      ]
+)
